3808a110a9ca76ce30ca9d529e850af5c7e9efbc
txx <2856873434@qq.com>
2025-01-15 10:20:53 +0800

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,32 +1,29 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/leetcode-diary/",
-  title: "LeetCode Diary",
-  description: "...",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-    search: {
-      provider: 'local'
-    },
-
-    sidebar: [
-      {
-        text: '全部题目',
-        items: [
-          { text: '1. 两数之和', link: '/0001' },
-          { text: '2. 两数相加', link: '/api-examples' }
+    base: "/leetcode-diary/",
+    title: "LeetCode Diary",
+    description: "...",
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            {text: '首页', link: '/'},
+        ],
+        search: {
+            provider: 'local'
+        },
+        sidebar: [
+            {
+                text: '全部题目',
+                items: [
+                    {text: '1. 两数之和', link: '/0001'},
+                    {text: '2. 两数相加', link: '/api-examples'}
+                ]
+            }
+        ],
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/tangxiangxiang/leetcode-diary'}
         ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/tangxiangxiang/leetcode-diary' }
-    ]
-  }
+    }
 })
